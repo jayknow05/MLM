8f58dc45fe969dcec960a244910f1ca8c958a976
jayknow05 <justin@justins-mbp.home>
2014-01-11 15:48:00 -0500

    Added Numberjack Integration
    I’ve added the Numberjack library which calls a Mistral solver. This
    does most of the heavy lifting involved with the optimization. Things
    should still be tightened up a bit, but I’m fairly confident in the
    solutions.

--- DataCollector/DataCollector/spiders/FanDuelSpider.py
+++ DataCollector/DataCollector/spiders/FanDuelSpider.py
@@ -7,6 +7,7 @@ import itertools
 import time
 from math import *
 from optimize import *
+from Numberjack import *
 
 from DataCollector.items import PlayerItem
 
@@ -29,17 +30,16 @@ class FanDuelSpider(BaseSpider):
             player['position'] = playerFullData[0].strip(' "\'\t\r\n')
             player['salary'] = int(playerFullData[5].strip(' "\'\t\r\n'))
             player['projection'] = 0
-            player['gradient'] = []
             
             players.append(player)
-
+        print "NUMBER OF PLAYERS: " + str(len(players))
         request = Request("https://www.numberfire.com/nba/fantasy/fantasy-basketball-projections", callback=self.parseNumberFire)
         request.meta['players'] = players
         return request
 
     def parseNumberFire(self, response):
 
-        alias = {'Bradley Beal' : 'Brad Beal', 'Phil Pressey' : 'Phil (Flip) Pressey'}
+        alias = {'Bradley Beal' : 'Brad Beal', 'Phil Pressey' : 'Phil (Flip) Pressey', 'Viacheslav Kravtsov' : 'Vyacheslav Kravtsov', 'MarShon Brooks' : 'Marshon Brooks'}
         notFound = []
         
         players = response.meta['players']
@@ -70,262 +70,97 @@ class FanDuelSpider(BaseSpider):
         print "Double check that these players aren't available for tonight's game:"
         print notFound
 
-        optimizeRoster(players)
+        players = playerSort(players)
+        self.optimize(['Mistral'], players)
+
+    def optimize(self, libs, players):
+
+        PGs = players.prunedByPosition['PG']
+        PGprojection = [ int(player['projection']*100) for player in players.prunedByPosition['PG']]
+        PGsalary = [ int(player['salary']) for player in players.prunedByPosition['PG']]
+        SGs = players.prunedByPosition['SG']
+        SGprojection = [ int(player['projection']*100) for player in players.prunedByPosition['SG']]
+        SGsalary = [ int(player['salary']) for player in players.prunedByPosition['SG']]
+
+        PFs = players.prunedByPosition['PF']
+        PFprojection = [ int(player['projection']*100) for player in players.prunedByPosition['PF']]
+        PFsalary = [ int(player['salary']) for player in players.prunedByPosition['PF']]
+        SFs = players.prunedByPosition['SF']
+        SFprojection = [ int(player['projection']*100) for player in players.prunedByPosition['SF']]
+        SFsalary = [ int(player['salary']) for player in players.prunedByPosition['SF']]
+
+        Cs = players.prunedByPosition['C']
+        Cprojection = [ int(player['projection']*100) for player in players.prunedByPosition['C']]
+        Csalary = [ int(player['salary']) for player in players.prunedByPosition['C']]
+
+        PG = VarArray(len(PGs))
+        SG = VarArray(len(SGs))
+        PF = VarArray(len(PFs))
+        SF = VarArray(len(SFs))
+        C = VarArray(len(Cs))
 
 
+        salaryCap = 60000
+        totalSalary = Variable(0, 1000000)
+        totalProjection = Variable(0, 1000000)
 
-    #     sel = Selector(response)
-    #     allPlayersFullData = sel.xpath('//script/text()').re('FD\.playerpicker\.allPlayersFullData \= \{(.*)\}')
-    #     allPlayersFullData = re.findall(r"\[([^\]]+)\]", allPlayersFullData[0])
+        model = Model(
+            Sum(PG) == 2,
+            Sum(SG) == 2,
+            Sum(PF) == 2,
+            Sum(SF) == 2,
+            Sum(C) == 1,
+            Sum(PG, PGprojection) + Sum(SG, SGprojection) + Sum(PF, PFprojection) + Sum(SF, SFprojection) + Sum(C, Cprojection) == totalProjection,
+            Sum(PG, PGsalary) + Sum(SG, SGsalary) + Sum(PF, PFsalary) + Sum(SF, SFsalary) + Sum(C, Csalary) == totalSalary,
+            totalSalary <= salaryCap,
+            Maximise(totalProjection)
+            )
 
-    #     players = []
-
-
-    #     for playerFullData in allPlayersFullData:
-    #         player = PlayerItem()
-    #         playerFullData = playerFullData.split(',')
-    #         player['name'] = playerFullData[1].strip(' "\'\t\r\n')
-    #         player['position'] = playerFullData[0].strip(' "\'\t\r\n')
-    #         player['salary'] = int(playerFullData[5].strip(' "\'\t\r\n'))
-    #         player['average'] = float(playerFullData[6].strip(' "\'\t\r\n'))
+        for library in libs:
             
+            solver = model.load(library) # Load up model into solver
+            solver.setVerbosity(1)
+            solver.setTimeLimit(600)
+            solver.setHeuristic('MinDomain', 'Random', 10)
 
-    #         # else:
-    #         #     continue
-
-    #         players.append(player)
-
-    #     request = Request("https://www.numberfire.com/nba/fantasy/fantasy-basketball-projections", callback=self.parseProjections)
-    #     request.meta['players'] = players
-    #     return request
-
-    # def selectTeam(self, players):
-        
-    #     PGs = []
-    #     SGs = []
-    #     SFs = []
-    #     PFs = []
-    #     Cs = []
-
-    #     for player in players:
-    #         pos = player['position']
-    #         if pos == 'PG':
-    #             PGs.append(player)
-    #         elif pos == 'SG':
-    #             SGs.append(player)
-    #         elif pos == 'SF':
-    #             SFs.append(player)
-    #         elif pos == 'PF':
-    #             PFs.append(player)
-    #         elif pos == 'C':
-    #             Cs.append(player)
-
-        
-    #     prunedPGs = self.analyzePlayers(PGs) 
-    #     prunedSGs = self.analyzePlayers(SGs) 
-    #     prunedSFs = self.analyzePlayers(SFs) 
-    #     prunedPFs = self.analyzePlayers(PFs) 
-    #     prunedCs = self.analyzePlayers(Cs)
-
-    #     PG1 = prunedPGs.pop(0)
-    #     PG2 = prunedPGs.pop(0)
-    #     SG1 = prunedSGs.pop(0)
-    #     SG2 = prunedSGs.pop(0)
-    #     SF1 = prunedSFs.pop(0)
-    #     SF2 = prunedSFs.pop(0)
-    #     PF1 = prunedPFs.pop(0)
-    #     PF2 = prunedPFs.pop(0)
-    #     C = prunedCs.pop(0)
-
-    #     nextPG = prunedPGs.pop(0)
-    #     nextSG = prunedSGs.pop(0)
-    #     nextSF = prunedSFs.pop(0)
-    #     nextPF = prunedPFs.pop(0)
-    #     nextC = prunedCs.pop(0)
-
-    #     totalSalary = PG1['salary'] + PG2['salary'] + SG1['salary'] + SG2['salary'] + SF1['salary'] + SF2['salary'] + PF1['salary'] + PF2['salary'] + C['salary'] 
-
-    #     gradientList = [( PG1['salary'] - nextPG['salary']) / (PG1['average'] - nextPG['average']), (PG2['salary'] - nextPG['salary']) / (PG2['average'] - nextPG['average']), ( SG1['salary'] - nextSG['salary']) / (SG1['average'] - nextSG['average']), (SG2['salary'] - nextSG['salary']) / (SG2['average'] - nextSG['average']), ( SF1['salary'] - nextSF['salary']) / (SF1['average'] - nextSF['average']), (SF2['salary'] - nextSF['salary']) / (SF2['average'] - nextSF['average']), ( PF1['salary'] - nextPF['salary']) / (PF1['average'] - nextPF['average']), (PF2['salary'] - nextPF['salary']) / (PF2['average'] - nextPF['average']), (C['salary'] - nextC['salary']) / (C['average'] - nextC['average']) ]
-
-    #     deltaAverageList = [ PG1['average'] - nextPG['average'], PG2['average'] - nextPG['average'], SG1['average'] - nextSG['average'], SG2['average'] - nextSG['average'], SF1['average'] - nextSF['average'], SF2['average'] - nextSF['average'], PF1['average'] - nextPF['average'], PF2['average'] - nextPF['average'], C['average'] - nextC['average'] ]
-
-    #     salaryCap = 60000
-
-    #     while totalSalary > salaryCap:
-    #         margin = totalSalary - salaryCap
-    #         testArray = []
-    #         for index, gradient in enumerate(gradientList):
-    #             if gradient >= margin:
-    #                 testArray.append(index)
+            solver.solve() # Now Solve
             
-    #         bestOption = gradientList.index(max(gradientList))
+            roster = self.buildRoster(PGs, PG, SGs, SG, PFs, PF, SFs, SF, Cs, C)
+            print ''
+            for player in roster:
+                print player['position'] + ":\t" + player['name'] + "\t Projection: " + str(player['projection']) + "\t Salary: " + str(player['salary'])
+            rosterProjection = sum([ player['projection'] for player in roster ])
+            print ''
+            print 'Team Totals'
+            print 'Projection:', rosterProjection, ' Salary: $', totalSalary.solution()
+            print ''
 
-    #         if testArray:
-    #             testArray.append(gradientList.index(max(gradientList)))            
-    #             bestOption = deltaAverageList.index(min(deltaAverageList[index] for index, test in enumerate(testArray)))
+    def buildRoster(self, PGs, PG, SGs, SG, PFs, PF, SFs, SF, Cs, C):
+        roster = []
+        PG = re.sub('\[|\]', '', PG.solution())
+        PG = PG.split(',')
+        for index, boolean in enumerate(PG):
+            if int(boolean) == 1:
+                roster.append(PGs[index]) 
+        SG = re.sub('\[|\]', '', SG.solution())
+        SG = SG.split(',')
+        for index, boolean in enumerate(SG):
+            if int(boolean) == 1:
+                roster.append(SGs[index]) 
+        PF = re.sub('\[|\]', '', PF.solution())
+        PF = PF.split(',')
+        for index, boolean in enumerate(PF):
+            if int(boolean) == 1:
+                roster.append(PFs[index]) 
+        SF = re.sub('\[|\]', '', SF.solution())
+        SF = SF.split(',')
+        for index, boolean in enumerate(SF):
+            if int(boolean) == 1:
+                roster.append(SFs[index]) 
+        C = re.sub('\[|\]', '', C.solution())
+        C = C.split(',')
+        for index, boolean in enumerate(C):
+            if int(boolean) == 1:
+                roster.append(Cs[index]) 
 
-    #         if bestOption == 0:
-    #             PG1 = nextPG
-    #             nextPG = prunedPGs.pop(0)
-    #             gradientList[0] = ( PG1['salary'] - nextPG['salary']) / (PG1['average'] - nextPG['average'])
-    #             gradientList[1] = ( PG2['salary'] - nextPG['salary']) / (PG2['average'] - nextPG['average'])
-    #             deltaAverageList[0] = PG1['average'] - nextPG['average']
-    #             deltaAverageList[1] = PG2['average'] - nextPG['average']
-    #         elif bestOption == 1:
-    #             PG2 = nextPG
-    #             nextPG = prunedPGs.pop(0)
-    #             gradientList[0] = ( PG1['salary'] - nextPG['salary']) / (PG1['average'] - nextPG['average'])
-    #             gradientList[1] = ( PG2['salary'] - nextPG['salary']) / (PG2['average'] - nextPG['average'])
-    #             deltaAverageList[0] = PG1['average'] - nextPG['average']
-    #             deltaAverageList[1] = PG2['average'] - nextPG['average']
-    #         elif bestOption == 2:
-    #             SG1 = nextSG
-    #             nextSG = prunedSGs.pop(0)
-    #             gradientList[2] = ( SG1['salary'] - nextSG['salary']) / (SG1['average'] - nextSG['average'])
-    #             gradientList[3] = ( SG2['salary'] - nextSG['salary']) / (SG2['average'] - nextSG['average'])
-    #             deltaAverageList[2] = SG1['average'] - nextSG['average']
-    #             deltaAverageList[3] = SG2['average'] - nextSG['average']
-    #         elif bestOption == 3:
-    #             SG2 = nextSG
-    #             try:
-    #                 nextSG = prunedSGs.pop(0)
-    #                 gradientList[2] = ( SG1['salary'] - nextSG['salary']) / (SG1['average'] - nextSG['average'])
-    #                 gradientList[3] = ( SG2['salary'] - nextSG['salary']) / (SG2['average'] - nextSG['average'])
-    #                 deltaAverageList[2] = SG1['average'] - nextSG['average']
-    #                 deltaAverageList[3] = SG2['average'] - nextSG['average']
-    #             except:
-    #                 gradientList[2] = 0
-    #                 gradientList[3] = 0
-    #                 deltaAverageList[2] = 1000
-    #                 deltaAverageList[3] = 1000                    
-    #         elif bestOption == 4:
-    #             SF1 = nextSF
-    #             try:
-    #                 nextSF = prunedSFs.pop(0)
-    #                 gradientList[4] = ( SF1['salary'] - nextSF['salary']) / (SF1['average'] - nextSF['average'])
-    #                 gradientList[5] = ( SF2['salary'] - nextSF['salary']) / (SF2['average'] - nextSF['average'])
-    #                 deltaAverageList[4] = SF1['average'] - nextSF['average']
-    #                 deltaAverageList[5] = SF2['average'] - nextSF['average']
-    #             except:
-    #                 gradientList[4] = 0
-    #                 gradientList[5] = 0
-    #                 deltaAverageList[4] = 1000
-    #                 deltaAverageList[5] = 1000
-    #         elif bestOption == 5:
-    #             SF2 = nextSF
-    #             try:
-    #                 nextSF = prunedSFs.pop(0)
-    #                 gradientList[4] = ( SF1['salary'] - nextSF['salary']) / (SF1['average'] - nextSF['average'])
-    #                 gradientList[5] = ( SF2['salary'] - nextSF['salary']) / (SF2['average'] - nextSF['average'])    
-    #                 deltaAverageList[4] = SF1['average'] - nextSF['average']
-    #                 deltaAverageList[5] = SF2['average'] - nextSF['average'] 
-    #             except:
-    #                 gradientList[4] = 0
-    #                 gradientList[5] = 0
-    #                 deltaAverageList[4] = 1000
-    #                 deltaAverageList[5] = 1000
-     
-    #         elif bestOption == 6:
-    #             PF1 = nextPF
-    #             nextPF = prunedPFs.pop(0)
-    #             gradientList[6] = ( PF1['salary'] - nextPF['salary']) / (PF1['average'] - nextPF['average'])
-    #             gradientList[7] = ( PF2['salary'] - nextPF['salary']) / (PF2['average'] - nextPF['average'])   
-    #             deltaAverageList[6] = PF1['average'] - nextPF['average']
-    #             deltaAverageList[7] = PF2['average'] - nextPF['average']        
-    #         elif bestOption == 7:
-    #             PF2 = nextPF
-    #             nextPF = prunedPFs.pop(0)
-    #             gradientList[6] = ( PF1['salary'] - nextPF['salary']) / (PF1['average'] - nextPF['average'])
-    #             gradientList[7] = ( PF2['salary'] - nextPF['salary']) / (PF2['average'] - nextPF['average'])  
-    #             deltaAverageList[6] = PF1['average'] - nextPF['average']
-    #             deltaAverageList[7] = PF2['average'] - nextPF['average']                     
-    #         else:
-    #             C = nextC
-    #             nextC = prunedCs.pop(0)
-    #             gradientList[8] = ( C['salary'] - nextC['salary']) / (C['average'] - nextC['average']) 
-    #             deltaAverageList[8] = C['average'] - nextC['average']         
-         
-
-    #         totalSalary = PG1['salary'] + PG2['salary'] + SG1['salary'] + SG2['salary'] + SF1['salary'] + SF2['salary'] + PF1['salary'] + PF2['salary'] + C['salary']
-    
-    #     totalScore = PG1['average'] + PG2['average'] + SG1['average'] + SG2['average'] + SF1['average'] + SF2['average'] + PF1['average'] + PF2['average'] + C['average']
-        
-    #     print PG1
-    #     print PG2
-    #     print SG1
-    #     print SG2
-    #     print SF1
-    #     print SF2
-    #     print PF1
-    #     print PF2
-    #     print C    
-    #     print "Total salary: $" + str(totalSalary) + " out of a possible $" + str(salaryCap)  
-    #     print "Average total score: " + str(totalScore)
-
-    # def parseProjections(self, response):
-    #     filename = "numberfire.html"
-    #     open(filename, 'wb').write(response.body)
-        
-    #     sel = Selector(response)
-    #     playerData = sel.xpath('//script/text()')
-
-    #     allPlayersNameID = playerData.re('"id":"([^"]+)","name":"([^"]+)","slug"')
-
-    #     allScores = playerData.re('(?<="draft_kings_fp":)([^,]+),')
-    #     allIDs = playerData.re('(?<="nba_player_id":")([^"]+)"')
-
-    #     playerNameID = {}
-    #     playerScores = {}
-
-    #     for index, NameID in enumerate(allPlayersNameID):
-    #         if index % 2:
-    #             Name = NameID
-    #             try:
-    #                 playerNameID[ID]
-    #                 print "Multiple players with the same ID found"
-    #             except:
-    #                 playerNameID[ID] = Name
-                    
-    #         else:
-    #             ID = NameID
-
-
-    #     for index, ID in enumerate(allIDs):
-    #         playerScores[playerNameID[ID]] = allScores[index]
-
-    #     players = response.meta['players']
-
-    #     name_indexer = dict((player['name'], i) for i, player in enumerate(players))
-
-    #     for key, value in playerScores.iteritems():
-    #         index = name_indexer.get(key, -1)
-    #         if index > -1:
-    #             players[index]['average'] = float(value)
-
-    #         else:
-    #             print "Can't find " + key + ", is the spelling consistent?"
-        
-    #     self.selectTeam(players)
-
-
-    # def analyzePlayers(self, playerList):
-    #     sortedPlayerList = sorted(playerList, key=itemgetter('average'), reverse=True)
-    #     prunedPlayerList = []
-
-    #     for player in sortedPlayerList:
-    #         try:
-    #             if lastSalary <= player['salary']:
-    #                 # print "Removing " + player['name'] + " in favor of " + lastPlayer
-    #                 continue
-    #             else:
-    #                 if lastAverage == player['average']:
-    #                     del prunedPlayerList[-1]
-    #                 prunedPlayerList.append(player)
-    #                 lastSalary = player['salary']
-    #                 lastAverage = player['average']
-    #         except:
-    #             prunedPlayerList.append(player)
-    #             lastSalary = player['salary']
-    #             lastAverage = player['average']
-
-    #     return prunedPlayerList
+        return roster 
--- DataCollector/DataCollector/spiders/optimize.py
+++ DataCollector/DataCollector/spiders/optimize.py
@@ -1,57 +1,19 @@
 from operator import itemgetter
 
-def optimizeRoster(players):
-    players = playerSort(players)
-    players.gradient(20000)
-    print players.byPosition['PG'][10]
-    # print gradient(players.pg, 10000, 0)
-
-class Roster(object):
-    def __init__(self, players):
-        self.players = players
-        self.pg = [ players[1], players[2] ]
-        self.sg = [ players[3], players[4] ]
-        self.pf = [ players[5], players[6] ]
-        self.sf = [ players[7], players[8] ]
-        self.c = players[9]
-    def salary(self):
-        return sum( [ player['salary'] for player in self.players ] )
-    def projection(self):
-        return sum( [ player['projection'] for player in self.players ] )
-    def margin(self, salaryCap):
-        return self.salary() - salaryCap
-
 class playerSort(object):
     def __init__(self, playerList):
         playerList = sorted(playerList, key=itemgetter('projection'), reverse=True)
         self.positionList = set( [player['position'] for player in playerList] )
         self.byPosition = {}
+        self.prunedByPosition = {}
         for pos in self.positionList:
             self.byPosition[pos] = [ player for player in playerList if player['position'] == pos ]
-
-    def gradient(self, capMargin):
-    
-        byPlayer = {}
-       
-        for pos in self.positionList:
-            
-            for currPlayer in self.byPosition[pos]:
-                for nextPlayer in self.byPosition[pos]:
-
-                    s1 = nextPlayer['salary']
-                    s2 = currPlayer['salary']
-                    
-                    if abs(capMargin) < abs(s1 - s2):
-                        salarySaved = capMargin
-                    else:
-                        salarySaved = s1 - s2
-
-                    p1 = nextPlayer['projection']
-                    p2 = currPlayer['projection']
-
-                    pointCost = p1 - p2
-                    if pointCost == 0:
-                        pointCost = 0.0001
-
-                    gradient = salarySaved/pointCost
-                    currPlayer['gradient'].append(gradient)        
+            for i, player in enumerate(self.byPosition[pos]):
+                if i == 0:
+                    self.prunedByPosition[pos] = [ player ]
+                    lastSalary = player['salary']
+                    lastProjection = player['projection']
+                elif ( player['salary'] < lastSalary ) and ( player['projection'] > 0 ):
+                    self.prunedByPosition[pos].append(player)
+                    lastSalary = player['salary']
+                    lastProjection = player['projection']       
